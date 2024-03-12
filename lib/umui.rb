f15b56fab9decf51db36bd22908ec15831142d6f
Sky Wang <skyjun.wang@qq.com>
2024-03-12 20:09:24 +0800

    fix error

--- lib/umui.rb
+++ lib/umui.rb
@@ -2,8 +2,8 @@
 require "yaml"
 require "umui/version"
 require "umui/constant"
-require 'active_resource'
-require 'jwt'
+require "active_resource"
+require "jwt"
 
 module Umui
   class Member < ActiveResource::Base
@@ -12,11 +12,11 @@ module Umui
     self.prefix = "/api/v1/members"
 
     def register params
-      callAPI("post" , "register" , params)
+      callAPI(:post , "register" , params)
     end
 
     def login params
-      callAPI("post" , "login" , params)
+      callAPI(:post , "login" , params)
     end
 
     def validate_token params
@@ -34,40 +34,40 @@ module Umui
 
         is_expired = DateTime.parse(strDateTime) < DateTime.now if !strDateTime.blank?
 
-        return format_result 0 , "SUCCESS" if !is_expired.nil? && !is_expired
+        return format_result(0 , "SUCCESS") if !is_expired.nil? && !is_expired
       rescue Exception => e
-        puts e.message
+        Log.error e.message
       end
 
-      callAPI("post" , "validate_token" , params)
+      callAPI(:post , "validate_token" , params)
     end
 
     def logout params
-      callAPI("post" , "logout" , params)
+      callAPI(:post , "logout" , params)
     end
 
     def clean_tokens params
-      callAPI("post" , "clean_tokens" , params)
+      callAPI(:post , "clean_tokens" , params)
     end
 
     def update params
-      callAPI("post" , "update" , params)
+      callAPI(:post , "update" , params)
     end
 
     def change_password params
-      callAPI("post" , "change_password" , params)
+      callAPI(:post , "change_password" , params)
     end
 
     def reset_password params
-      callAPI("post" , "reset_password" , params)
+      callAPI(:post , "reset_password" , params)
     end
 
     def enable params
-      callAPI("post" , "enable" , params)
+      callAPI(:post , "enable" , params)
     end
 
     def disable params
-      callAPI("post" , "disable" , params)
+      callAPI(:post , "disable" , params)
     end
 
     def validate_invitation_code params
@@ -79,18 +79,22 @@ module Umui
     end
 
     def check_mobile_existence(params)
-      callAPI('get', 'check_mobile_existence', params)
+      callAPI(:get, "check_mobile_existence", params)
+    end
+
+    def search_by_uids(params)
+      callAPI(:get, :search_by_uids, params)
     end
 
 private
     def get_uid_decrypt_token token
       begin
         info = JWT.decode token , SERVICE_CONFIG["secret_key"]
-        uid = info[0]["uid"]   if !info.blank?
-        result = format_result 0, "SUCCESS", uid             if !info.blank?
-        result = format_result 30024 , "ERR_TOKEN_INVALID"   if  info.blank?
+        uid  = info[0]["uid"]   if info.present?
+        result = format_result(0, "SUCCESS", uid)           if info.present?
+        result = format_result(30024 , "ERR_TOKEN_INVALID") if info.blank?
       rescue Exception => e
-        puts e.message
+        Log.error e.message
         result = format_result 30024 , "ERR_TOKEN_INVALID"
       end
 
@@ -98,33 +102,28 @@ private
     end
 
     def callAPI(post_or_get , method , params)
-      result = {}
-
       begin
-        if post_or_get == "post"
+        if post_or_get == :get
+          result = Member.get(method, params)
+        else
           result_response = Member.post(method, params)
           result_body = JSON.parse result_response.body if result_response
           result = result_body if result_body
         end
-
-        if post_or_get == "get"
-          result = Member.get(method, params)
-        end
-
       rescue Exception => e
         Log.error e
-        result = format_result 10001 , e.message
+        result = format_result(10001 , e.message)
       end
 
       result
     end
 
-    def format_result code, info, uid=""
-      result = {}
-      result['return_code'] = code if !code.blank?
-      result['return_info'] = info if !info.blank?
-      result['uid']         = uid  if !uid.blank?
-      result
+    def format_result(code, info, uid="")
+      {}.tap do | result |
+        result["return_code"] = code
+        result["return_info"] = info
+        result["uid"]         = uid
+      end
     end
 
   end
